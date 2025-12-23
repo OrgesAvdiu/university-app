988ac9b069fa03fd55ab9f742ae02021379659e9
Orges Avdiu <orgesiav@gmail.com>
2025-12-23 10:39:22 +0100

    Remove CORS_ALLOWED_ORIGINS; keep only CORS_ALLOW_ALL_ORIGINS

--- university_api/university_api/settings.py
+++ university_api/university_api/settings.py
@@ -93,11 +93,6 @@ REST_FRAMEWORK = {
 }
 
 # CORS
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'https://university-app-orges.vercel.app',
-]
 CORS_ALLOW_CREDENTIALS = False
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_HEADERS = [
