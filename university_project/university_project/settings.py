5aaad9c0dd580259f69fa3491d1442894b1b3f31
Orges Avdiu <orgesiav@gmail.com>
2025-12-23 08:50:54 +0100

    Prepare for PythonAnywhere deployment

--- university_project/university_project/settings.py
+++ university_project/university_project/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'django-insecure-07+z2-@c40rzl#^3ep67(%sr7zw0y=*^9$64bi$!k05fc5k)tn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['orges.pythonanywhere.com', 'www.orges.pythonanywhere.com']
-print("ALLOWED_HOSTS loaded:", ALLOWED_HOSTS)
-
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -116,8 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
